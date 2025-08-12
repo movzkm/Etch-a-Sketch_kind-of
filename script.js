64ed6d20e9109181a3e73e7e1c7de67a328a08c3
movzkm <movzkm@gmail.com>
2025-08-12 10:28:42 +0300

    getting the mouse input

--- script.js
+++ script.js
@@ -7,15 +7,25 @@ const rock = 3;
 const rows = 50;
 const columns = 50;
 
+let currentX = 0;
+let currentY = 0;
+
 const grid = [];
 
-const fallingContainer = document.querySelector("fallingContainer");
+const canvas = document.querySelector("#canvas");
+const context = canvas.getContext("2d");
 
 /*
 logic:
 - we have a total of rows * columns boxes
 - when the player press on the are specified an object is inserted
-- 
+- and the logic for the particles start
+
+problems:
+- how to create the divs
+- how to access them
+
+i guess we just need to use canvas
 */
 
 function createGrid() {
@@ -31,3 +41,28 @@ function createGrid() {
 }
 
 createGrid();
+
+let mouseDown = false;
+
+function printMouseLocation() {
+    if (mouseDown) {
+        console.log("x: " + currentX + " y: " + currentY);
+    }
+}
+
+document.querySelector("#canvas").addEventListener("mousedown", event => {
+    currentX = event.clientX;
+    currentY = event.clientY;
+    mouseDown = true;
+})
+
+document.querySelector("#canvas").addEventListener("mouseup", event => {
+    mouseDown = false;
+})
+
+document.querySelector("#canvas").addEventListener("mousemove", event => {
+    if (mouseDown) {
+        currentX = event.clientX;
+        currentY = event.clientY;
+    }
+})
