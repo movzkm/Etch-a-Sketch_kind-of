429f950c6a228d01977aadf59200fe26872537b1
movzkm <movzkm@gmail.com>
2025-08-12 11:54:52 +0300

    validating the mouse input

--- script.js
+++ script.js
@@ -9,23 +9,31 @@ const columns = 50;
 
 let currentX = 0;
 let currentY = 0;
+let mouseDown = false;
+let validMouseInput = false;
 
 const grid = [];
+const stack = [];
 
 const canvas = document.querySelector("#canvas");
+const boundingRect = canvas.getBoundingClientRect();
+const minX = Math.floor(boundingRect.left);
+const minY = Math.floor(boundingRect.top);
+const maxX = Math.floor(boundingRect.right);
+const maxY = Math.floor(boundingRect.bottom);
+
 const context = canvas.getContext("2d");
+
+const inputChecker = document.querySelector("#inputChecker");
 
 /*
 logic:
 - we have a total of rows * columns boxes
-- when the player press on the are specified an object is inserted
+- when the player press on the area specified an object is inserted
 - and the logic for the particles start
+- a stack containing the objects inserted and their positions
+- the stack is used to update the positions of objects and the grid
 
-problems:
-- how to create the divs
-- how to access them
-
-i guess we just need to use canvas
 */
 
 function createGrid() {
@@ -40,29 +48,44 @@ function createGrid() {
    }
 }
 
-createGrid();
+function setPosition(x, y) {
 
-let mouseDown = false;
-
-function printMouseLocation() {
-    if (mouseDown) {
-        console.log("x: " + currentX + " y: " + currentY);
+    if (mouseDown && minX <= x && x <= maxX 
+        && minY <= y && y <= maxY) {
+        currentX = x - minX;
+        currentY = y - minY;
+        mouseDown = true;
+        validMouseInput = true;
+    }
+    else {
+        validMouseInput = false;
     }
 }
 
-document.querySelector("#canvas").addEventListener("mousedown", event => {
-    currentX = event.clientX;
-    currentY = event.clientY;
+document.querySelector("html").addEventListener("mousedown", event => {
     mouseDown = true;
+    setPosition(event.clientX, event.clientY);
+    update();
 })
 
-document.querySelector("#canvas").addEventListener("mouseup", event => {
+document.querySelector("html").addEventListener("mouseup", event => {
     mouseDown = false;
+    validMouseInput = false;
 })
 
-document.querySelector("#canvas").addEventListener("mousemove", event => {
-    if (mouseDown) {
-        currentX = event.clientX;
-        currentY = event.clientY;
-    }
+document.querySelector("html").addEventListener("mousemove", event => {
+    setPosition(event.clientX, event.clientY);
 })
+
+
+function update() {
+    if (validMouseInput) {
+        inputChecker.textContent = "valid";
+    }
+    else {
+        inputChecker.textContent = "INVALID";
+    }
+}
+
+createGrid();
+setInterval(update, 250);
